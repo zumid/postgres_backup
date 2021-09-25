10d4014442b4362721529fda2e335de1b76468bd
zumi <zumi@zumid.net>
2021-09-26 01:20:37 +0900

    [update] db money
    money データベースをバックアップするように変更

--- postgres_backup.sh
+++ postgres_backup.sh
@@ -2,7 +2,7 @@
 
 #### Settings ####
 
-DBNAME="meshi"
+DBNAMES=("meshi" "money")
 DBUSER="postgres"
 BACKUPDIR="./backup"
 DATE=`date "+%Y%m%d"`
@@ -23,43 +23,48 @@ cd `dirname $0`
 
 logging " INFO" "Backup Process start"
 
-logging " INFO" "pg_dump start"
-
-pg_dump -U ${DBUSER} -Fp ${DBNAME} > ${BACKUPDIR}/${DBNAME}_${DATE}.sql
-if [ $? -ne 0 ];then
-	logging "ERROR" "pg_dump failed"
-	logging "ERROR" "backup stopped"
-	exit
-fi
-
-logging " INFO" "pg_dump finished"
-
-logging " INFO" "gzip start"
+for DBNAME in "${DBNAMES[@]}"
+do
 
 
-if [ ! -f "${BACKUPDIR}/${DBNAME}_${DATE}.sql.gz" ]; then
-	gzip ${BACKUPDIR}/${DBNAME}_${DATE}.sql | tee -a ${LOGDIR}/`basename $0 .sh`_`date "+%Y%m%d"`.log
-	if [ $? -ne 0 ];then
-		logging "ERROR" "gzip failed"
-	fi
-else
-	logging " WARN" "gzip skipped"
-fi
-logging " INFO" "gzip finished"
+    logging " INFO" "pg_dump start [${DBNAME}]"
 
-logging " INFO" "dropbox upload start"
-/opt/scripts/dropbox_uploader/dropbox_uploader.sh upload ${BACKUPDIR}/${DBNAME}_${DATE}.sql.gz `uname -n`/postgres_backup/ | tee -a ${LOGDIR}/`basename $0 .sh`_`date "+%Y%m%d"`.log
-if [ $? -ne 0 ];then
-	logging "ERROR" "dropbox upload failed"
-	exit
-fi
-logging " INFO" "dropbox upload finished"
+    pg_dump -U ${DBUSER} -Fp ${DBNAME} > ${BACKUPDIR}/${DBNAME}_${DATE}.sql
+    if [ $? -ne 0 ];then
+        logging "ERROR" "pg_dump failed"
+        logging "ERROR" "backup stopped"
+        exit
+    fi
 
-logging " INFO" "delete old backup start"
-find ${BACKUPDIR} -type f -daystart -mtime ${BACKUP_PERIOD} -exec rm {} \; | tee -a ${LOGDIR}/`basename $0 .sh`_`date "+%Y%m%d"`.log
-if [ $? -ne 0 ];then
-	logging "ERROR" "delete old backup failed"
-fi
+    logging " INFO" "pg_dump finished"
 
-logging " INFO" "delete old backup finished"
-logging " INFO" "All Backup Process finished"
+    logging " INFO" "gzip start"
+
+
+    if [ ! -f "${BACKUPDIR}/${DBNAME}_${DATE}.sql.gz" ]; then
+        gzip ${BACKUPDIR}/${DBNAME}_${DATE}.sql | tee -a ${LOGDIR}/`basename $0 .sh`_`date "+%Y%m%d"`.log
+        if [ $? -ne 0 ];then
+            logging "ERROR" "gzip failed"
+        fi
+    else
+        logging " WARN" "gzip skipped"
+    fi
+    logging " INFO" "gzip finished"
+
+    logging " INFO" "dropbox upload start"
+    /opt/scripts/dropbox_uploader/dropbox_uploader.sh upload ${BACKUPDIR}/${DBNAME}_${DATE}.sql.gz `uname -n`/postgres_backup/ | tee -a ${LOGDIR}/`basename $0 .sh`_`date "+%Y%m%d"`.log
+    if [ $? -ne 0 ];then
+        logging "ERROR" "dropbox upload failed"
+        exit
+    fi
+    logging " INFO" "dropbox upload finished"
+
+    logging " INFO" "delete old backup start"
+    find ${BACKUPDIR} -type f -name "*.gz" -daystart -mtime ${BACKUP_PERIOD} -exec rm {} \; | tee -a ${LOGDIR}/`basename $0 .sh`_`date "+%Y%m%d"`.log
+    if [ $? -ne 0 ];then
+        logging "ERROR" "delete old backup failed"
+    fi
+
+    logging " INFO" "delete old backup finished"
+    logging " INFO" "All Backup Process finished"
+done
